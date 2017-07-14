0033a72e24cae5a8e6aafbbb21c545f2c73cbb07
J. Antonio Salomon <josesalomon@gmail.com>
2017-07-14 12:26:42 -0500

    arreglo insurer

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -1,6 +1,6 @@
 class Claim < ApplicationRecord
 
-  belongs_to :insurer
+  
   belongs_to :doctor
   belongs_to :patient
   belongs_to :event, :required => false
--- app/models/insurer.rb
+++ app/models/insurer.rb
@@ -4,5 +4,5 @@ class Insurer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_many :claims
+        
 end
