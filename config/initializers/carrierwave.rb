61c48381cd530ce70787deeb6e958eeba7163b83
J. Antonio Salomon <josesalomon@gmail.com>
2017-09-05 18:24:18 -0500

    fog3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 # if Rails.env.production?
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'
+
   config.fog_credentials = {
     # Configuration for Amazon S3 should be made available through an Environment variable.
     # For local installations, export the env variable through the shell OR
@@ -15,6 +15,8 @@ CarrierWave.configure do |config|
     :aws_access_key_id     => 'AKIAJU3XPOE5OX524K3Q',
     :aws_secret_access_key => 'o3vTOcsoRckXfTunA9wc19vgXupA5uywV4btm8iP',
     :region                => 'us-east-2'
+    # host:                  's3.example.com',             # optional, defaults to nil
+    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
   #config.fog_directory = ENV['S3_BUCKET']
   # For testing, upload files to local `tmp` folder.
@@ -29,7 +31,7 @@ CarrierWave.configure do |config|
    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
   #
    config.fog_directory    = 'medilinxmx'
-   config.fog_use_ssl_for_aws = false                          # Generate http:// urls. Defaults to :authenticated_read (https://)
-   config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
+   config.fog_public     = false                             # Generate http:// urls. Defaults to :authenticated_read (https://)
+   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} # optional, defaults to {}
 end
 # end
