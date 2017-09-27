5f6ebdd962c85c0833dad61ad75d376b7af96b10
J. Antonio Salomon <josesalomon@gmail.com>
2017-09-27 00:08:04 -0500

    prueba

--- db/migrate/20170919042621_add_credential_fields_again.rb
+++ db/migrate/20170919042621_add_credential_fields_again.rb
@@ -1,15 +1,5 @@
 class AddCredentialFieldsAgain < ActiveRecord::Migration[5.0]
   def change
-    add_column :credentials, :email, :string
-    add_column :credentials, :phone_work, :string
-    add_column :credentials, :phone_mobile, :string
-    add_column :credentials, :street_address_work, :string
-    add_column :credentials, :neighborhood_work, :string
-    add_column :credentials, :district_work, :string
-    add_column :credentials, :zip_work, :string
-    add_column :credentials, :city_work, :string
-    add_column :credentials, :state_work, :string
-    add_column :credentials, :practice_name, :string
-    add_column :credentials, :permission_public, :boolean
+
   end
 end
