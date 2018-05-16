5d8a650f144f74216292af1943f747010108bafb
jasalomont <josesalomon@gmail.com>
2018-05-16 10:44:39 -0500

    date medical rel

--- db/migrate/20180515162021_add_fields_to_medical_rel.rb
+++ db/migrate/20180515162021_add_fields_to_medical_rel.rb
@@ -4,7 +4,7 @@ class AddFieldsToMedicalRel < ActiveRecord::Migration[5.0]
     add_column :medical_relations, :first_name, :string
     add_column :medical_relations, :last_father, :string
     add_column :medical_relations, :Last_mother, :string
-    add_column :medical_relations, :birthdate, :string
+    add_column :medical_relations, :birthdate, :date
     add_column :office_profiles, :first_name, :string
     add_column :office_profiles, :last_father, :string
     add_column :office_profiles, :Last_mother, :string
