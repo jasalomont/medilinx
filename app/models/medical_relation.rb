0ecf00b9d0309b38701d3f354c13bcfc23bf5d27
jasalomont <josesalomon@gmail.com>
2018-05-17 13:19:24 -0500

    ARREGLO MODELO

--- app/models/medical_relation.rb
+++ app/models/medical_relation.rb
@@ -3,8 +3,7 @@ class MedicalRelation < ApplicationRecord
 # validates :patient_id, :presence => true, :uniqueness=> {:scope => :doctor_id}
 
 belongs_to :doctor
-belongs_to :patient
-belongs_to :office_profile
+
 
 has_one :credential, :through => :doctor, :source => :credentials
 #
